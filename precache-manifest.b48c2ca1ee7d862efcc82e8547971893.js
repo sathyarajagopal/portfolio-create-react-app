3df98c213605a3eff3a5dae42241b0feebad9368
SPR <smartaxgenius@gmail.com>
2020-03-08 16:36:44 -0400

    Updates

---
+++ precache-manifest.b48c2ca1ee7d862efcc82e8547971893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03ade23bb898cf3ac440263b08e504b",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "9cda2088bc385c657330",
+    "url": "/portfolio-create-react-app/static/css/main.3180bdec.chunk.css"
+  },
+  {
+    "revision": "9546f443ddcb27550f39",
+    "url": "/portfolio-create-react-app/static/js/2.abb2cf71.chunk.js"
+  },
+  {
+    "revision": "9cda2088bc385c657330",
+    "url": "/portfolio-create-react-app/static/js/main.e1d607c8.chunk.js"
+  },
+  {
+    "revision": "7307fc9a229ef17d0c73",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.3bdfb6bc.js"
+  }
+]);
